09befd2ed49e89da54f57d5aceb4924745f0099a
Sana Batool <125119037+Sanabatool@users.noreply.github.com>
2024-12-31 21:53:51 +0500

    Add files via upload

---
+++ portfolio/main.js
@@ -0,0 +1,50 @@
+// var typed = new typed(".text", {
+//     Strings: ["Fullstack developer", "python programmer", "canvas"],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backDelay: 1000,
+//     loop: true
+// });
+
+//for mobile screen menu icon
+document.getElementById('menu-icon').addEventListener('click', function() {
+  var navbar = document.querySelector('.navbar');
+  navbar.classList.toggle('active');
+});
+
+//for scrolling from header
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150; // Adjust the offset as needed
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    }
+  });
+};
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
